51fa0c476eb61e17ad08436d0eca12b2a955d797
xluan1 <shortshort1m6@gmail.com>
2024-02-01 17:02:29 +0700

    refactor ICurrentUserService

--- src/main/java/com/xuanluan/mc/sdk/authenticate/service/ICurrentUserService.java
+++ src/main/java/com/xuanluan/mc/sdk/authenticate/service/ICurrentUserService.java
@@ -5,5 +5,7 @@ import com.xuanluan.mc.sdk.authenticate.domain.model.CurrentUser;
 import java.util.function.Supplier;
 
 public interface ICurrentUserService {
-    CurrentUser get(String clientId, String token, Supplier<CurrentUser> userSupplier);
+    CurrentUser get(String clientId, String token);
+
+    CurrentUser putIfAbsent(String clientId, String token, Supplier<CurrentUser> userSupplier);
 }
--- src/main/java/com/xuanluan/mc/sdk/authenticate/service/impl/CurrentUserServiceImpl.java
+++ src/main/java/com/xuanluan/mc/sdk/authenticate/service/impl/CurrentUserServiceImpl.java
@@ -3,19 +3,26 @@ package com.xuanluan.mc.sdk.authenticate.service.impl;
 import com.xuanluan.mc.sdk.authenticate.domain.model.CurrentUser;
 import com.xuanluan.mc.sdk.authenticate.service.ICurrentUserService;
 import com.xuanluan.mc.sdk.authenticate.service.constant.CacheNameConstant;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.util.Assert;
+import com.xuanluan.mc.sdk.service.builder.CacheBuilder;
+import com.xuanluan.mc.sdk.utils.StringUtils;
+import org.springframework.cache.CacheManager;
 
 import java.util.function.Supplier;
 
-@CacheConfig(cacheNames = CacheNameConstant.currentUser, cacheManager = "currentUserCacheManager")
 public class CurrentUserServiceImpl implements ICurrentUserService {
-    @Cacheable(key = "#clientId+'/'+#token")
+    private final CacheBuilder<CurrentUser> cacheBuilder;
+
+    public CurrentUserServiceImpl(CacheManager cacheManager) {
+        cacheBuilder = new CacheBuilder<>(cacheManager, CacheNameConstant.currentUser, CurrentUser.class);
+    }
+
     @Override
-    public CurrentUser get(String clientId, String token, Supplier<CurrentUser> userSupplier) {
-        CurrentUser currentUser = userSupplier.get();
-        Assert.notNull(currentUser, "current user must not null");
-        return currentUser;
+    public CurrentUser get(String clientId, String token) {
+        return cacheBuilder.get(StringUtils.toKey(clientId, token));
+    }
+
+    @Override
+    public CurrentUser putIfAbsent(String clientId, String token, Supplier<CurrentUser> userSupplier) {
+        return cacheBuilder.putIfAbsent(StringUtils.toKey(clientId, token), userSupplier);
     }
 }
