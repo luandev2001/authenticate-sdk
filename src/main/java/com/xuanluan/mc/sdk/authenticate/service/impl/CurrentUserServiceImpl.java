da1f1e075e35f9e3534db84a9e5a7def54989ef7
xluan <ngoxuanluan2001@gmail.com>
2024-08-19 00:21:45 +0700

    remove CacheBuilder

--- src/main/java/com/xuanluan/mc/sdk/authenticate/service/impl/CurrentUserServiceImpl.java
+++ src/main/java/com/xuanluan/mc/sdk/authenticate/service/impl/CurrentUserServiceImpl.java
@@ -3,26 +3,33 @@ package com.xuanluan.mc.sdk.authenticate.service.impl;
 import com.xuanluan.mc.sdk.authenticate.domain.model.CurrentUser;
 import com.xuanluan.mc.sdk.authenticate.service.ICurrentUserService;
 import com.xuanluan.mc.sdk.authenticate.service.constant.CacheNameConstant;
-import com.xuanluan.mc.sdk.service.builder.CacheBuilder;
-import com.xuanluan.mc.sdk.utils.StringUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 
 import java.util.function.Supplier;
 
+@RequiredArgsConstructor
 public class CurrentUserServiceImpl implements ICurrentUserService {
-    private final CacheBuilder<CurrentUser> cacheBuilder;
-
-    public CurrentUserServiceImpl(CacheManager cacheManager) {
-        cacheBuilder = new CacheBuilder<>(cacheManager, CacheNameConstant.currentUser, CurrentUser.class);
-    }
+    private final CacheManager cacheManager;
 
     @Override
     public CurrentUser get(String clientId, String token) {
-        return cacheBuilder.get(StringUtils.toKey(clientId, token));
+        return getCache().get(getKeyCache(clientId, token), CurrentUser.class);
     }
 
     @Override
     public CurrentUser putIfAbsent(String clientId, String token, Supplier<CurrentUser> userSupplier) {
-        return cacheBuilder.putIfAbsent(StringUtils.toKey(clientId, token), userSupplier);
+        CurrentUser currentUser = get(clientId, token);
+        if (currentUser == null) getCache().put(getKeyCache(clientId, token), userSupplier.get());
+        return currentUser;
+    }
+
+    private String getKeyCache(String clientId, String token) {
+        return String.join(":", clientId, token);
+    }
+
+    private Cache getCache() {
+        return cacheManager.getCache(CacheNameConstant.currentUser);
     }
 }
