97fcb6c2b3a1546a01d1c1a29d6d578961477f14
xluan <ngoxuanluan2001@gmail.com>
2024-09-03 13:21:08 +0700

    not auto create CacheUserConfig

--- src/main/java/com/xuanluan/mc/sdk/authenticate/config/CacheUserConfig.java
+++ src/main/java/com/xuanluan/mc/sdk/authenticate/config/CacheUserConfig.java
@@ -4,15 +4,11 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.xuanluan.mc.sdk.config.BaseCacheConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
-@Configuration
-@EnableCaching
 public class CacheUserConfig extends BaseCacheConfig {
 
     @Value("${cache.user.expire_time:120}")
